a7a8a27c128eeec1e117fbe9e7af031431c13415
dipidup <agrawal.ankit1295@gmail.com>
2019-11-16 00:12:49 +0530

    bg change for section 1

---
+++ js/preloader.js
@@ -0,0 +1,8 @@
+// Because only Chrome supports offset-path, feGaussianBlur for now
+
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+
+if(!isChrome) {
+    document.getElementsByClassName('infinityChrome')[0].style.display = "none";
+    document.getElementsByClassName('infinity')[0].style.display = "block";
+}
